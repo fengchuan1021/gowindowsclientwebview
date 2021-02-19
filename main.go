7090d7a87d30f025f0f2c70e903ea65826851e4c
fengchuan <1299693999@qq.com>
2021-02-19 18:39:07 +0800

    ddd

--- main.go
+++ main.go
@@ -1,33 +1,55 @@
 package main
-import "github.com/yamnikov-oleg/wingo"
-func  onclose(w *wingo.Window) bool{
+
+import (
+	"fmt"
+
+	"github.com/webview/webview"
+	"github.com/yamnikov-oleg/wingo"
+	"unsafe"
+)
+
+func onclose(w *wingo.Window) bool {
+	fmt.Println("whhy")
 	w.Hide()
 	return false
 }
-func ontrayclick(w *wingo.Window){
+func ontrayclick(w *wingo.Window) {
 	w.Show()
 }
-func ontrayrightclick(w *wingo.Window){
-traymenu.StartContext(w)
+func ontrayrightclick(w *wingo.Window) {
+	traymenu.StartContext(w)
 
 }
+func oncreate(w *wingo.Window) {
+	fmt.Println("111111")
+	hand := w.GetHandle()
+	wv := webview.NewWindow(true, unsafe.Pointer(&hand))
+	wv.Navigate("http://www.baidu.com")
+	wv.Run()
+	defer wv.Destroy()
+}
+
 var traymenu *wingo.Menu
 
-func main(){
-	w:=wingo.NewWindow(true,true)
-	size:=wingo.Vector{500,500}
+func main() {
+	w := wingo.NewWindow(true, true)
+	size := wingo.Vector{500, 500}
 	w.SetSize(size)
-	w.OnClose=onclose
-	w.OnTrayClick=ontrayclick
-	w.OnTrayRightClick=ontrayrightclick
-	icon:=wingo.LoadIcon(101)
+	w.OnClose = onclose
+	w.OnTrayClick = ontrayclick
+	w.OnTrayRightClick = ontrayrightclick
+	//w.OnCreate=oncreate
+	icon := wingo.LoadIcon(101)
 	w.SetIcon(icon)
-	w.AddTrayIcon(icon,"hellofromaiel")
-	traymenu=wingo.NewContextMenu()
-	exitbtn:=traymenu.AppendItemText("退出")
-	exitbtn.OnClick= func(item *wingo.MenuItem) {
+	w.AddTrayIcon(icon, "hellofromaiel")
+	traymenu = wingo.NewContextMenu()
+	exitbtn := traymenu.AppendItemText("退出")
+	exitbtn.OnClick = func(item *wingo.MenuItem) {
 		w.Destroy()
 	}
+
+	oncreate(w)
 	w.Show()
+
 	wingo.Start()
 }
