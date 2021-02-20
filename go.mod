00846b69fe84967167e3e3a1483414e9fe0f5718
fengchuan <1299693999@qq.com>
2021-02-20 09:42:20 +0800

    ddd

---
+++ go.mod
@@ -0,0 +1,11 @@
+module awesomeProject1
+
+go 1.15
+
+require (
+	github.com/elazarl/go-bindata-assetfs v1.0.1 // indirect
+	github.com/webview/webview v0.0.0-20210216142346-e0bfdf0e5d90 // indirect
+	github.com/yamnikov-oleg/w32 v0.0.0-20151008150751-b1984dac6e87 // indirect
+	github.com/yamnikov-oleg/wingo v0.0.0-20151008151310-5f5970bcdae8 // indirect
+	github.com/zserge/lorca v0.1.9 // indirect
+)
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"awesomeProject1/data"
 	"fmt"
-
+	assetfs "github.com/elazarl/go-bindata-assetfs"
 	"github.com/webview/webview"
 	"github.com/yamnikov-oleg/wingo"
+	"net"
+	"net/http"
 	"unsafe"
 )
 
@@ -20,18 +23,51 @@ func ontrayrightclick(w *wingo.Window) {
 	traymenu.StartContext(w)
 
 }
-func oncreate(w *wingo.Window) {
-	fmt.Println("111111")
+func oncreate(w *wingo.Window, url string) {
+
 	hand := w.GetHandle()
 	wv := webview.NewWindow(true, unsafe.Pointer(&hand))
-	wv.Navigate("http://www.baidu.com")
+	wv.Navigate(url + "/static/index.html")
 	wv.Run()
 	defer wv.Destroy()
 }
 
 var traymenu *wingo.Menu
 
+func myhttp(urlchan chan string) {
+	mux := http.NewServeMux()
+
+	files := assetfs.AssetFS{
+		Asset:     data.Asset,
+		AssetDir:  data.AssetDir,
+		AssetInfo: data.AssetInfo,
+		Prefix:    "",
+	}
+	mux.Handle("/", http.FileServer(&files))
+	//mux.HandleFunc("/start", start)
+	//mux.HandleFunc("/frame", getFrame)
+	//mux.HandleFunc("/key", captureKeys)
+
+	// get an ephemeral port, so we're guaranteed not to conflict with anything else
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		panic(err)
+	}
+	portAddress := listener.Addr().String()
+	urlchan <- "http://" + portAddress
+	listener.Close()
+	server := &http.Server{
+		Addr:    portAddress,
+		Handler: mux,
+	}
+	server.ListenAndServe()
+}
+
 func main() {
+	urlchan := make(chan string)
+	go myhttp(urlchan)
+	prefix := <-urlchan
+	fmt.Println(prefix)
 	w := wingo.NewWindow(true, true)
 	size := wingo.Vector{500, 500}
 	w.SetSize(size)
@@ -48,7 +84,7 @@ func main() {
 		w.Destroy()
 	}
 
-	oncreate(w)
+	oncreate(w, prefix)
 	w.Show()
 
 	wingo.Start()
